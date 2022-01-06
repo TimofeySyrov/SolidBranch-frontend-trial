6e5181c7f0a3306b4ca0b817c5e050e113f004c7
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-01-06 17:12:45 +0300

    Added site routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NavigatorComponent } from './navigator/navigator.component';
+import { SummaryComponent } from './summary/summary.component';
+
+const routes: Routes = [
+  { path: 'navigator', component: NavigatorComponent },
+  { path: '', component: SummaryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
