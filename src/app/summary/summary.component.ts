249e2e3750fc5ed912e218616cb234aba9cd8a17
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-01-06 21:11:37 +0300

    Added dynamic display of user types and total

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import IUser from 'src/services/user/IUser';
+import IUsersType from 'src/services/user/IUsersType';
+import { UserService } from '../../services/user/user.service';
+
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent implements OnInit {
+  users: IUser[] = [];
+  usersTypeList: IUsersType[] = [];
+  allUsersNumber: number = 0;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.getUsers();
+    this.getUsersTypeList();
+    this.getAllUsersNumber();
   }
 
+  getUsers(): void {
+    this.users = this.userService.getUsers();
+  }
+
+  getUsersTypeList(): void {
+    this.usersTypeList = this.userService.getTypeUsersList();
+  }
+
+  getAllUsersNumber(): void {
+    this.allUsersNumber = this.userService.getAllUsersNumber();
+  }
 }
