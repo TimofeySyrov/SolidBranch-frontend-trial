b1ee3b0c0b923897c8eca8cbe1112abe1664bc2f
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-01-06 21:09:55 +0300

    Fixed using json data. Added 'getTypeUsersList' and 'getUsersByType' methods.

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 
 import dataUsersList from '../../assets/dataUsersList.json';
 import IUser from './IUser';
+import IUsersType from './IUsersType';
 import TUser from './TUser';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private users: IUser[] = JSON.parse(JSON.stringify(dataUsersList));
+  private usersJson = JSON.parse(JSON.stringify(dataUsersList));
+  private users: IUser[] = this.usersJson.data;
 
   constructor() {}
 
   getUsers(): IUser[] {
-    return this.users;
+    return this.usersJson.data;
   }
 
   getUser(id: string): IUser {
@@ -23,14 +25,30 @@ export class UserService {
   }
 
   getAllUsersNumber(): number {
-    return this.users.length;
+    return this.usersJson.total;
   }
 
-  getTypeUsersNumber(usersType: TUser) {
+  getTypeUsersList(): IUsersType[] {
+    const income = this.users.filter(user => user.type === 'income');
+    const investment = this.users.filter(user => user.type === 'investment');
+    const loan = this.users.filter(user => user.type === 'loan');
+    const outcome = this.users.filter(user => user.type === 'outcome');
+
+    return [
+      { id: 0, name: 'income', number: income.length },
+      { id: 1, name: 'investment', number: investment.length },
+      { id: 2, name: 'loan', number: loan.length },
+      { id: 3, name: 'outcome', number: outcome.length },
+    ];
+  }
+
+  getUsersByType(typeName: TUser): IUser[] | null {
     const currentUsersList = this.users.filter((user) => {
-      return user.type === usersType;
+      return user.type === typeName;
     });
 
-    return currentUsersList.length;
+    const hasUsersByType = currentUsersList.length > 0;
+
+    return hasUsersByType ? currentUsersList : null;
   }
 }
